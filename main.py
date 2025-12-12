3ea59abfbf8128fb00b64b93517e9b506cc7fb35
cillegio8 <65701325+cillegio8@users.noreply.github.com>
2025-12-13 00:09:23 +0400

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,12 @@ app = FastAPI(title="AI-Bina AVM API (MVP)")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"^https://.*\.local-credentialless\.webcontainer-api\.io$",
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["*"],
-    max_age=86400,
 )
+
 
 
 # =========================================================
